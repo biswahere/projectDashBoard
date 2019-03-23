6f8f761cf98d15260c24da45a7463d1b3c828e77
biswahere <43244983+biswahere@users.noreply.github.com>
2019-03-23 23:43:38 +0530

    Update ProjectController.java

--- project-service/src/main/java/com/biswatech/project/controller/ProjectController.java
+++ project-service/src/main/java/com/biswatech/project/controller/ProjectController.java
@@ -1,7 +1,19 @@
 package com.biswatech.project.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,20 +21,49 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.biswatech.project.model.Project;
 import com.biswatech.project.proxy.DbServiceProxyforProjectService;
+import com.biswatech.project.service.MapValidationErrorService;
 
 @RestController
 @RequestMapping("/rest/project")
+@CrossOrigin
 public class ProjectController {
 	
 	@Autowired
 	private DbServiceProxyforProjectService dbServiceProxyforProjectService;
 	
-	@PostMapping("/createProject")
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
 	
-	public ResponseEntity<Project> saveorUpdate(@RequestBody Project project) {
+	@PostMapping("/createProject")
+	public ResponseEntity<?> saveorUpdate( @Valid @RequestBody Project project, BindingResult result) {
 		
+		ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationErrorService(result);
+		if(errorMap != null) return errorMap;
+				
 		return this.dbServiceProxyforProjectService.createNewProject(project);
 		
+	}
+	
+	@GetMapping("/project/{id}")
+	public Project findProjectById(@PathVariable Long id){
+		
+		return dbServiceProxyforProjectService.fetchProject(id);
+		
+	}
+	
+	@GetMapping("/projectList")
+	public List<Project> findAllProjects(){
+		
+		return dbServiceProxyforProjectService.getAllProjects();
+		
+	}
+	
+	@DeleteMapping("/deleteProject/{id}")
+	public void deleteProject(@PathVariable Long id){
+		
+	 this.dbServiceProxyforProjectService.deleteProject(id);
+	
+
 	}
 
 }
