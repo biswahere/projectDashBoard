a5e3335019ec6a13fcbfc3877442d793fa81db78
biswahere <43244983+biswahere@users.noreply.github.com>
2019-03-23 23:44:13 +0530

    Update DbServiceProxyforProjectService.java

--- project-service/src/main/java/com/biswatech/project/proxy/DbServiceProxyforProjectService.java
+++ project-service/src/main/java/com/biswatech/project/proxy/DbServiceProxyforProjectService.java
@@ -1,7 +1,12 @@
 package com.biswatech.project.proxy;
 
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,5 +19,18 @@ public interface DbServiceProxyforProjectService {
 	
 	@PostMapping("/api/project/create")
 	public ResponseEntity<Project> createNewProject(@RequestBody Project project);
+	
+	
+
+	@GetMapping("/api/project/fetchProject/{id}")
+	public Project fetchProject( @PathVariable Long id);
+	
+	@GetMapping("/api/project/fetchProjectList")
+	public List<Project> getAllProjects();
+	
+	@DeleteMapping("/api/project/{id}")
+	public void deleteProject(@PathVariable Long id);
+	
+	
 
 }
