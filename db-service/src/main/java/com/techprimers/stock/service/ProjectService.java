da9ce8b5a57e48fd27ceee4faaa3080915f9bf4f
biswahere <43244983+biswahere@users.noreply.github.com>
2019-03-23 23:41:54 +0530

    Update ProjectService.java

--- db-service/src/main/java/com/techprimers/stock/service/ProjectService.java
+++ db-service/src/main/java/com/techprimers/stock/service/ProjectService.java
@@ -1,6 +1,9 @@
 package com.techprimers.stock.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.techprimers.stock.model.Project;
@@ -17,6 +20,28 @@ public class ProjectService {
 		//Logic
 	
 		return projectRepository.save(project);
+	}
+
+	public Project findById(Long id) {
+		
+		return projectRepository.getOne(id);
+	}
+
+	public List<Project> findAllProjects() {
+		
+		return projectRepository.findAll();
+	}
+
+	public void deleteProject(Long id) {
+		
+		Project project = this.projectRepository.getOne(id);
+		
+		/*if (project == null) {
+			 throw new ProjectIdExcepion();
+		}*/
+		projectRepository.delete(project);
+		
+		
 	}
 
 }
