9a78c32622ba116d683827a7d38d7b8786be2f5a
biswahere <43244983+biswahere@users.noreply.github.com>
2019-03-23 23:40:21 +0530

    Update ProjectController.java

--- db-service/src/main/java/com/techprimers/stock/resource/ProjectController.java
+++ db-service/src/main/java/com/techprimers/stock/resource/ProjectController.java
@@ -1,9 +1,18 @@
 package com.techprimers.stock.resource;
 
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +23,44 @@ import com.techprimers.stock.service.ProjectService;
 
 @RestController
 @RequestMapping("/api/project")
+@CrossOrigin
 public class ProjectController {
 	
 	@Autowired
 	private ProjectService projectService;
 	
 	@PostMapping("/create")
-	public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+		/*if (result.hasErrors()) {
+			
+			return new ResponseEntity<String>("Invalid Project", HttpStatus.BAD_REQUEST);
+		}
+		*/
 		this.projectService.saveOrUpdate(project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/fetchProject/{id}")
+	public Project fetchProject(@PathVariable Long id ) {
+		
+		System.out.println("id"+id);
+		return projectService.findById(id);
+		
+	}
+	
+	@GetMapping("/fetchProjectList")
+	public List<Project> getAllProjects() {
+	
+		return projectService.findAllProjects();
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteProject(@PathVariable Long id) {
+	
+		this.projectService.deleteProject(id);
+		
+		
 	}
 	
 
