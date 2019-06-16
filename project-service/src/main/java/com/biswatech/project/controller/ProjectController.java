35fb5b31ac7acafc656c9723221a1077304c535f
Biswajit Bhandari <biswajitb731130@gmail.com>
2019-06-16 11:41:54 +0530

    changes in project controller

--- project-service/src/main/java/com/biswatech/project/controller/ProjectController.java
+++ project-service/src/main/java/com/biswatech/project/controller/ProjectController.java
@@ -39,7 +39,8 @@ public class ProjectController {
 		
 		ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationErrorService(result);
 		if(errorMap != null) return errorMap;
-				
+		System.out.println("jhhgfgfgcfgcfgcvg ");
+	
 		return this.dbServiceProxyforProjectService.createNewProject(project);
 		
 	}
